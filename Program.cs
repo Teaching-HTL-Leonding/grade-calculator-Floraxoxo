3624a7e97a50f6a83ef166ba745244b3392db6e7
Floraxoxo <114140834+Floraxoxo@users.noreply.github.com>
2022-10-20 11:51:16 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int points = 0;
-int points_possible = 0;
+int points_possible = 20;
 
 Console.WriteLine("Have you participated in the first written exam? [Yes/No]");
 string exam1 = Console.ReadLine()!;
 Console.WriteLine("Have you participated in the second written exam? [Yes/No]");
 string exam2 = Console.ReadLine()!;
+Console.WriteLine("How much points did you get through homework? 1-20");
+int homework_points = int.Parse(Console.ReadLine()!);
+points += homework_points;
+Console.WriteLine("Did you take an oral exam? [Yes/No]");
+string oral_exam = Console.ReadLine()!;
 
 if (exam1 == "yes" || exam1 == "Yes")
 {
-    Console.WriteLine("How much points did you get? 1-20 ");
+    Console.WriteLine("How much points did you get in the first exam? 1-20 ");
     points += int.Parse(Console.ReadLine()!);
     points_possible += 20;
 }
 
 if (exam2 == "yes" || exam2 == "Yes")
 {
-    Console.WriteLine("How much points did you get? 1-20 ");
+    Console.WriteLine("How much points did you get in the second exam? 1-20 ");
+    points += int.Parse(Console.ReadLine()!);
+    points_possible += 20;
+}
+if (oral_exam == "yes" || oral_exam == "Yes")
+{
+    Console.WriteLine("How much points did you get in the oral exam? 1-20 ");
     points += int.Parse(Console.ReadLine()!);
     points_possible += 20;
 }
@@ -43,3 +54,4 @@ else
 {
     Console.WriteLine("Sorry you didn't pass");
 }
+
